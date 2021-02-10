ff98a4f570a207706d5babb152a1c9d5b92892c1
Yara <58008773+volodiny71299@users.noreply.github.com>
2021-02-10 14:41:10 +1300

    pepega

--- 02_ticket_loop_v2.py
+++ 02_ticket_loop_v2.py
@@ -2,17 +2,22 @@
 
 # initial loop so that it runs at least once
 name = ""
-count = 1
+count = 0
 MAX_TICKETS = 5
 
 while name != "xxx" and count < MAX_TICKETS:
+    print("You have {} seats left".format(MAX_TICKETS - count))
 
+    print("Counter: ", count)
     # get details
     name = input("Name: ")
-    if name == "xxx":
-        print("You have sold {} tickets There are {} places still available".format(count, MAX_TICKETS - count))
 
-    else:
-        print("You have {} seats left".format(MAX_TICKETS - count))
+    print()
+
+    if name == "xxx":
+        print("You have sold {} tickets\nThere are {} places still available".format(count, MAX_TICKETS - count))
+
     count += 1
 
+if count == MAX_TICKETS:
+    print("You have sold all the tickets available")
