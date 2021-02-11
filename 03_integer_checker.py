25fdee2c1a20951c7433fb4b9918555fe7cb474a
Yara <58008773+volodiny71299@users.noreply.github.com>
2021-02-11 13:20:18 +1300

    starting number check

---
+++ 03_integer_checker.py
@@ -0,0 +1,19 @@
+# number checker
+
+
+def num_check(question, low, high):
+    valid = False
+    while not valid:
+        try:
+            response = int(input(question))
+            if low <= response <= high:
+                valid = True
+                return response
+            else:
+                print("Please enter a number between {} and {}\n".format(low, high))
+
+        except ValueError:
+            print("Invalid input")
+
+age = num_check("How old are you: ", 12, 130)
+print("\nYou are {} years old".format(age))
