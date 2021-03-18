3c6f28593b30bbe64cbd810a7caa8c09d79742b4
Yara <58008773+volodiny71299@users.noreply.github.com>
2021-03-18 13:19:51 +1300

    Checkpoint 4

---
+++ 08_dictionary.py
@@ -0,0 +1,25 @@
+# initialise snack lists...
+
+names = ['Rangi', 'Manaia', 'Talia', 'Arihi', 'Fetu']
+
+popcorn = []
+mms = []
+pita_chips = []
+water = []
+orange_juice = []
+
+snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
+
+snack_menu_dict = {
+    'Popcorn': popcorn,
+    'Pita Chips': pita_chips,
+    'Water': water,
+    'M&Ms': mms,
+    'Orange Juice': orange_juice,
+}
+
+test_data = [
+    [[1, 'Water'], [1, 'Pita Chips'], [1, 'M&Ms']],
+    [[1, 'Pita Chips'], [1, 'Orange Juice'], [1, 'M&Ms']],
+    [[1, 'Water']]
+]
