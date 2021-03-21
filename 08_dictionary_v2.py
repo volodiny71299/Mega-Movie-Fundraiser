3405dfd15c2a805f12373da439fa2793e1f357fc
Yara <58008773+volodiny71299@users.noreply.github.com>
2021-03-22 10:55:10 +1300

    09 lists

--- 08_dictionary_v2.py
+++ 08_dictionary_v2.py
@@ -12,9 +12,9 @@ snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
 
 snack_menu_dict = {
     'Popcorn': popcorn,
+    'M&Ms': mms,
     'Pita Chips': pita_chips,
     'Water': water,
-    'M&Ms': mms,
     'Orange Juice': orange_juice,
 }
 
@@ -23,7 +23,7 @@ test_data = [
     [[]],
     [[1, 'Water']],
     [[1, 'Popcorn'], [1, 'Orange Juice']],
-    [[1, 'Water'], [1, 'Pita Chips'], [3, 'Orange Juice']]
+    [[1, 'M&Ms'], [1, 'Pita Chips'], [3, 'Orange Juice']]
 ]
 
 count = 0
@@ -40,9 +40,17 @@ for client_order in test_data:
     count += 1
 
     for item in snack_order:
-        to_find = (item[1])
-        amount = (item[0])
-        add_list = snack_menu_dict[to_find]
-        add_list[-1] = amount
+        if len(item) > 0:
+            to_find = (item[1])
+            amount = (item[0])
+            add_list = snack_menu_dict[to_find]
+            add_list[-1] = amount
 
 print(snack_lists)
+
+print()
+print("Popcorn: ", snack_lists[0])
+print("M&Ms: ", snack_lists[1])
+print("Pita Chips: ", snack_lists[2])
+print("Water: ", snack_lists[3])
+print("Orange Juice: ", snack_lists[4])
